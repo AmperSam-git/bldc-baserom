8d7540637a4f64bbdf9e819cd4955b4258f1ef3c
AmperSam <81581391+AmperSam-git@users.noreply.github.com>
2023-03-09 14:51:06 -0330

    Move retry settings reset out of retry code

--- Tools/UberASMTool/gamemode/GM0C.asm
+++ Tools/UberASMTool/gamemode/GM0C.asm
@@ -1,3 +1,12 @@
 init:
     jsl retry_load_overworld_init
+
+
+    ; Reset some retry settings on return to overworld
+    incsrc "../retry_config/ram.asm"
+    incsrc "../retry_config/settings.asm"
+
+    ; Reset to the default retry prompt type on return to OW
+    lda.b #!default_prompt_type+1 : sta !ram_prompt_override
+
     rtl
